a81f5958447f8eacc67bbd7106ad83004c924b71
Aashutosh Uikey <aashureal01@gmail.com>
2025-08-14 15:37:44 +0530

    Change Store

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,7 @@
-import React, { useEffect } from "react";
-import { getUsers } from "../store/userAction";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
 const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getUsers());
-  }, []);
-
-  const users = useSelector((state) => state.users.value);
-  console.log(users);
-
-  return <div className=""></div>;
+  return <div>Home</div>;
 };
 
 export default Home;
