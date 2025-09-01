86d8f12de39ef64ed4e5df3694e84e718348b53c
Aashutosh Uikey <aashureal01@gmail.com>
2025-09-01 22:52:55 +0530

    re initialized

--- client/src/store/store.jsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import userSlice from "./reducers/userSlice";
-
-export const store = configureStore({
-  reducer: {
-    userReducer: userSlice,
-  },
-});
